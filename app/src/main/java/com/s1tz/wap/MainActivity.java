1eb7d478beca20eaa121bb3f329179804c693afd
jiangyou1 <351870651@qq.com>
2016-10-10 00:59:15 +0800

    添加了界面

--- app/src/main/java/com/s1tz/wap/MainActivity.java
+++ app/src/main/java/com/s1tz/wap/MainActivity.java
@@ -1,13 +1,26 @@
 package com.s1tz.wap;
 
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.s1tz.wap.tools.ImageLoader;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ImageView iv_first;
+
+    @RequiresApi(api = Build.VERSION_CODES.HONEYCOMB_MR1)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        iv_first = (ImageView) findViewById(R.id.iv_first);
+
+        ImageLoader imageLoader = new ImageLoader();
+        imageLoader.displayImage("https://ss0.bdstatic.com/94oJfD_bAAcT8t7mm9GUKT-xh_/timg?image&quality=100&size=b4000_4000&sec=1476028109&di=17bce5ac1a02f9a97a787cadc1285e98&src=http://photocdn.sohu.com/20111231/Img330861134.jpg", iv_first);
     }
 }
